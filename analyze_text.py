89d9a10483c4905587b3b8e7845fa672ea9c6927
theresaoh <49175564+theresaoh@users.noreply.github.com>
2019-04-26 19:10:27 -0600

    Update analyze_text.py

--- analyze_text.py
+++ analyze_text.py
@@ -1,4 +1,17 @@
 # Remember to reach out for help after your own due diligence
 
 def analyze_text(text):
-    # Your code here
+    e = 0
+    alphas = 0
+    for char in text:
+        if char == "e" or char == "E":
+            e += 1
+            alphas += 1
+        elif char.isalpha() == True:
+            alphas += 1
+    calculation = round(((e/alphas) * 100), 2)
+    percentage = "(" + str(calculation) + "%)"
+    alphastr = str(alphas)
+    estring = str(e)
+    result = str("The text contains " + alphastr + " alphabetic characters, of which " + estring + " " + percentage + " are 'e'.")
+    return result
